e9001944eebb4cd4055e848aa6427bd429523212
Shweta Jaiswal <01shwetajais@gmail.com>
2026-04-22 12:54:51 +0530

    Implement basic arithmetic operations in C
    Operators are the basic components of C programming. They are symbols that represent some kind of operation, such as mathematical, relational, bitwise, conditional, or logical computations, which are to be performed on values or variables. The values and variables used with operators are called operands.

---
+++ Basics_Operators1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    
+    // Expression for getting sum
+  //  int sum = 10 + 20;
+    
+   // printf("%d", sum);
+//_____________________________________________________________________________________________________
+  int a = 25, b = 5;
+
+    // using operators and printing results
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+    printf("+a = %d\n", +a);
+    printf("-a = %d\n", -a);
+    printf("a++ = %d\n", a++);
+    printf("a-- = %d\n", a--);
+    return 0;
+}
